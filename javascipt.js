595f416bbf1514bfeecdce054657aaa1eb01648c
Savely <136673461+Savelushechka@users.noreply.github.com>
2024-11-29 13:51:46 +0300

    Add files via upload

---
+++ javascipt.js
@@ -0,0 +1,7 @@
+function chek() {
+    var val_1 = document.getElementById("in_1")
+    var val_2 = document.getElementById("in_2")
+    var val_3 = document.getElementById("in_3")
+    var val_4 = document.getElementById("in_4")
+    var val_5 = document.getElementById("in_5")
+}
